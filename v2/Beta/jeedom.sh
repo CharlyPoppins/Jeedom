4666f9fb3c38ea0ea734ad1589b6e06539c1fbe3
megastorage <stephane.flavigny@gmail.com>
2016-03-11 23:47:12 +0100

    Update jeedom.sh

--- v2/Beta/jeedom.sh
+++ v2/Beta/jeedom.sh
@@ -4,7 +4,7 @@
 case "$1" in
    "start")
    /etc/init.d/rsyslog start
-   /etc/init.d/mtab.sh start
+   #/etc/init.d/mtab.sh start
    /etc/init.d/cron start
    /etc/init.d/apache2 start
    /etc/init.d/php5-fpm start
@@ -14,7 +14,7 @@ case "$1" in
    /etc/init.d/php5-fpm stop
    /etc/init.d/apache2 stop
    /etc/init.d/cron stop
-   /etc/init.d/mtab.sh stop
+   #/etc/init.d/mtab.sh stop
    /etc/init.d/rsyslog stop
    ;;
 esac
