1587549c05f78e1b6dd9d46975f5ccb5b872de4e
PuNiSHeR374 <xpunisher@live.be>
2016-03-14 10:03:23 +0100

    test

--- v2/Release/Syno/install_drivers_alias.sh
+++ v2/Release/Syno/install_drivers_alias.sh
@@ -13,4 +13,3 @@ ln -s /usr/local/debian-chroot/var/chroottarget/ /volume1/debian
 wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/jeedom.zip
 unzip jeedom.zip -d /volume1/@appstore
 ln -s /volume1/@appstore/jeedom/ui /usr/syno/synoman/webman/3rdparty/jeedom
-rm jeedom.zip
