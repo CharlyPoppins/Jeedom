dc25409d3444108bd2e9438c054609cd803b828a
PuNiSHeR374 <xpunisher@live.be>
2016-03-14 09:59:14 +0100

    Update

--- v2/Release/Syno/install_drivers_alias.sh
+++ v2/Release/Syno/install_drivers_alias.sh
@@ -3,7 +3,6 @@ wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/cp210x.ko
 wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/ftdi_sio.ko
 wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/usbserial.ko
 wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/S99Modules.sh
-
 mv *.ko /lib/modules
 mv S99Modules.sh /usr/syno/etc.defaults/rc.d/
 chmod 777 /usr/syno/etc.defaults/rc.d/S99Modules.sh
