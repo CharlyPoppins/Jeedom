955c78f4bcb0d45f647b08b4c4704adbb3d8e5f8
PuNiSHeR374 <xpunisher@live.be>
2016-03-17 14:06:49 +0100

    NodeJs_Install.sh

--- v2/Sources/PuNiSHeR/NodeJs_Install.sh
+++ v2/Sources/PuNiSHeR/NodeJs_Install.sh
@@ -1,8 +1,28 @@
+#!/bin/sh
 
-
+# Mise à jour des Paquets
 sudo apt-get update
+sudo apt-get install git-core curl build-essential openssl libssl-dev
 
 cd /tmp
 
+# Récupèration NodeJs depuis Github
 git clone https://github.com/joyent/node.git
+cd node
 
+# Récupération de la dernière version stable (actuellement 0.10.40)
+git checkout v0.10.40
+
+# Compilation et Installation
+./configure
+make
+sudo make install
+
+# Vérification de la Version NodeJs
+node -v
+
+# Installation NPM
+curl -L https://www.npmjs.org/install.sh | sh
+
+# Vérification de la Version NPM
+npm -v
