584075684dd50f9b2e522c4e119faa7910715f3f
PuNiSHeR374 <xpunisher@live.be>
2016-03-18 16:39:10 +0100

    Create NodeJs.sh

---
+++ v2/Sources/PuNiSHeR/NodeJs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+apt-get install sudo
+cd /tmp
+
+echo "Début de l'installation"
+
+actual=`nodejs -v`;
+
+echo "Version actuelle : ${actual}"
+
+if [[ $actual == *"4."* || $actual == *"5."* ]]
+then
+  echo "Ok, version suffisante";
+else
+  echo "KO, version obsolète à upgrader";
+  echo "Suppression du Nodejs existant et installation du paquet recommandé"
+  sudo apt-get -y --purge autoremove nodejs npm
+  arch=`arch`;
+  echo 30 > /tmp/mySensors_dep
+  if [[ $arch == "armv6l" ]]
+  then
+    echo "Raspberry 1 détecté, utilisation du paquet pour armv6"
+    sudo rm /etc/apt/sources.list.d/nodesource.list
+    wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+    sudo dpkg -i node_latest_armhf.deb
+    sudo ln -s /usr/local/bin/node /usr/local/bin/nodejs
+    rm node_latest_armhf.deb
+  else
+    echo "Utilisation du dépot officiel"
+    curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+  fi
+  new=`nodejs -v`;
+  echo "Version actuelle : ${new}"
+fi
+
+npm cache clean
+sudo npm cache clean
+
+echo "Fin de l'installation"
