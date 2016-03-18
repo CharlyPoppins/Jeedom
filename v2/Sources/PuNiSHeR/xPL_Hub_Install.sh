b8f3b431578046b18d28b5bf23883b62a3e80e02
PuNiSHeR374 <xpunisher@live.be>
2016-03-18 13:11:45 +0100

    Update

--- v2/Sources/PuNiSHeR/xPL_Hub_Install.sh
+++ v2/Sources/PuNiSHeR/xPL_Hub_Install.sh
@@ -1,10 +1,25 @@
+#!/bin/sh
+#
+# Commande pour vérifier l'écoute du Hub :
+# netstat -anp|grep 3865
+
+cd /tmp
+
+mkdir Hub_xPL
+
 wget http://www.xpl4java.org/xPL4Linux/downloads/xPLLib.tgz
 tar xvzf xPLLib.tgz
+
 cd xPLLib
+
 make
+
 sudo make install
+
 cd examples/
+
 make
-sudo cp xPL_Hub  xPL_Logger /usr/local/bin
- 
+
+sudo cp xPL_Hub xPL_Logger /usr/local/bin
+
 /usr/local/bin/xPL_Hub
