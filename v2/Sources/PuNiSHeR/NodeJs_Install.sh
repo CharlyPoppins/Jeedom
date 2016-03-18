a43390ecbf03204fc2e918a35e1fd520b17934ca
PuNiSHeR374 <xpunisher@live.be>
2016-03-18 15:13:32 +0100

    update

--- v2/Sources/PuNiSHeR/NodeJs_Install.sh
+++ v2/Sources/PuNiSHeR/NodeJs_Install.sh
@@ -3,14 +3,14 @@
 # Exécuter le Script en root
 #
 # Mise à jour des Paquets
-sudo apt-get update
-sudo apt-get install curl build-essential openssl libssl-dev
+apt-get update
+apt-get install curl build-essential openssl libssl-dev
 
 cd /tmp
 
 # Récupération de la dernière version stable
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
 
 # Vérification de la Version NodeJs
 node -v
