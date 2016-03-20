6736bdd1e2425aeb354285f502e4dd3690cfb4bb
PuNiSHeR374 <xpunisher@live.be>
2016-03-20 12:37:46 +0100

    Update install_x12.sh

--- v2/Beta/install_x12.sh
+++ v2/Beta/install_x12.sh
@@ -2,32 +2,21 @@
 #
 # Installateur Synology x12 Series
 #
-#!/bin/sh
 # SFY alias stef74
-# Version jeedom 2.1.1 en chroot synology
-# dans le chroot
+# Version jeedom 2.x.x en chroot synology
+#
+# Dans le chroot :
 # cd /tmp
-# wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Beta/install_mini.sh
+# wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Beta/install_x12.sh
 # chmod+x install_mini.sh
-# sh install_mini.sh
+# sh install_x12.sh
 # Le port 8088 doit être non utilisé
 # De preférence un chroot tout neuf avec un reboot du nas chroot a deja été installé.
 # Avoir installé les drivers usb soit manuellement soit par le spk http://www.jadahl.com/domoticz_beta/packages/UsbSerialDrivers_3.0.9.spk
 # Enocean don't work on 32bits
 # Lors de l'install de apache repondre N a la question.
 
-#status sous syno de apache2 et demarrage des services
-#cd /home
-#wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Beta/jeedom.sh
-#chmod +x jeedom.sh
-
-#recup config apache2 pour ne pas avoir de messages d'erreurs
-#Faut repondre N lors de la question à l'install de apache2
-cd /tmp
-# mkdir /etc/apache2/
-# wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Beta/apache2_x12.conf
-# mv /tmp/apache2_x12.conf /etc/apache2/apache2.conf
-
+install_dependency() {
 chmod 777 /dev/tty*
 apt-get update
 apt-get -y install locales
@@ -41,21 +30,52 @@ apt-get -y install curl
 apt-get -y install make
 apt-get -y install mc
 apt-get -y install vim
-apt-get -y install apache2
-apt-get -y install apache2.2-common
+apt-get -y install unzip
+apt-get -y install htop
+apt-get -y install nano
+}
+
+install_webserver() {
+apt-get install mysql-server 
+apt-get install mysql-client
+apt-get -y install nginx
+apt-get install php5-fpm
+apt-get -y install php5-curl
+apt-get -y install php5-dev
+apt-get -y install php5-json
+apt-get -y install php5-mysql
+apt-get -y install php5-ldap
+apt-get -y install php5-gd
+apt-get -y install php-pear
+apt-get -y install ca-certificates
+}
+
+# Status sous syno de Nginx et demarrage des services
+cd /home
+
+wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Beta/jeedom.sh
+chmod +x jeedom.sh
+
+#recup config apache2 pour ne pas avoir de messages d'erreurs
+#Faut repondre N lors de la question à l'install de apache2
+cd /tmp
+# mkdir /etc/apache2/
+# wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Beta/apache2_x12.conf
+# mv /tmp/apache2_x12.conf /etc/apache2/apache2.conf
 
 echo "export LANG=fr_FR.utf8" >> ~/.bashrc
 echo "export LC_ALL=fr_FR.utf8" >> ~/.bashrc
 
 echo "cd /home" >> ~/.bashrc
 
-# on se place dans le repertoire de travail
+# On se place dans le repertoire de travail
 cd /tmp
 
-#################################Perso########################################
-# on recupere la Version 2.1.1
-###############################################################################
+# Arret Service Apache pour déinstallation si présent
 service apache2 stop
+apt-get autoremove --purge apache2
+
+
 
 #wget https://raw.githubusercontent.com/jeedom/core/stable/install/apache_security -O /etc/apache2/conf-available/security.conf
 #rm /etc/apache2/conf-enabled/security.conf
