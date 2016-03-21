e2c9a8df6f7c819c303061ffc7c55c0bd36b48e9
PuNiSHeR374 <xpunisher@live.be>
2016-03-21 12:19:58 +0100

    update

--- v2/Beta/install_x12.sh
+++ v2/Beta/install_x12.sh
@@ -76,6 +76,8 @@ apt-get -y install nano
 install_webserver() {
 apt-get -y install mysql-client
 apt-get -y install nginx
+#apt-get install -y nginx-common
+#apt-get install -y nginx-full
 apt-get -y install php5-fpm
 apt-get -y install php5-curl
 apt-get -y install php5-dev
@@ -95,34 +97,34 @@ read answer
 
 if [ "$(echo $answer | grep "^[ [:digit:] ]*$")" ]
 then
-        if [ $answer -eq 80 ]; then
-                echo ""; echo "";
-                echo "Le Port 80 est deja utilise sur le Synology...";
-                echo "Veuillez en choisir une autre.";
-                configure_nginx
-        else
-                echo ""; echo "";
-                echo "Vous avez choisi le Port : "$answer;
-                echo "Changement de Port en Cours...";
-                echo ""; echo "";
-                sed -i 's/listen 80 default_server;/listen '"$answer"' default_server;/g' /etc/nginx/sites-en$
-                sed -i 's/listen \[\:\:\]\:80 default_server;/listen \[\:\:\]\:'"$answer"' default_server;/g'$
+	if [ $answer -eq 80 ]; then
+		echo ""; echo "";
+		echo "Le Port 80 est deja utilise sur le Synology...";
+		echo "Veuillez en choisir une autre.";
+		configure_nginx
+	else
+		echo ""; echo "";
+		echo "Vous avez choisi le Port : "$answer;
+		echo "Changement de Port en Cours...";
+		echo ""; echo "";
+		sed -i 's/listen 80 default_server;/listen '"$answer"' default_server;/g' /etc/nginx/sites-en$
+		sed -i 's/listen \[\:\:\]\:80 default_server;/listen \[\:\:\]\:'"$answer"' default_server;/g'$
 
-                cp /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+		cp /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
 
-                echo "Redemarrage de Nginx...";
-                service nginx stop
-                service nginx start
-        fi
+		echo "Redemarrage de Nginx...";
+		service nginx stop
+		service nginx start
+	fi
 elif [ -z $answer ]; then
-                echo ""; echo "";
-                echo "Vous ne pouvez pas laisser le Port vide...";
-                configure_nginx
+	echo ""; echo "";
+	echo "Vous ne pouvez pas laisser le Port vide...";
+	configure_nginx
 else
-        echo ""; echo "";
-        echo "Vous avez introduit : "$answer;
-        echo "Veuillez introduire que des chiffres.";
-        configure_nginx
+	echo ""; echo "";
+	echo "Vous avez introduit : "$answer;
+	echo "Veuillez introduire que des chiffres.";
+	configure_nginx
 fi
 }
 
@@ -197,48 +199,54 @@ wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom
 mv nginx_x12.conf /etc/nginx/default
 
 
-# Configuration Port Nginx
+# Configuration Port Virtualhost Nginx
 configure_nginx
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Status sous syno de Nginx et demarrage des services
+# Status sous syno de Nginx et demarrage des services Jeedom
 cd /home
+
+if [ -f "/home/jeedom.sh" ];then
+	rm /home/jeedom.sh
+fi
 
 wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Beta/jeedom_x12.sh
 mv jeedom_x12.sh jeedom.sh
 chmod +x jeedom.sh
 
+service nginx stop
 
+rm /var/www/html/index.html
 
-#wget https://raw.githubusercontent.com/jeedom/core/stable/install/apache_security -O /etc/apache2/conf-available/security.conf
-#rm /etc/apache2/conf-enabled/security.conf
-#ln -s /etc/apache2/conf-available/security.conf /etc/apache2/conf-enabled/
-
-#rm /var/www/html/index.html
-
+#config apache2
+#sed -i 's/max_execution_time = 30/max_execution_time = 600/g' /etc/php5/apache2/php.ini
+#sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1G/g' /etc/php5/apache2/php.ini
+#sed -i 's/post_max_size = 8M/post_max_size = 1G/g' /etc/php5/apache2/php.ini
+#sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/apache2/php.ini
+#sed -i 's/pm.max_children = 5/pm.max_children = 20/g' /etc/php5/fpm/pool.d/www.conf
 
 
 #config des droits	
 echo "www-data ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)
 
+
+#config du cron
+croncmd="su --shell=/bin/bash - www-data -c '/usr/bin/php /var/www/html/core/php/jeeCron.php' >> /dev/null"
+cronjob="* * * * * $croncmd"
+( crontab -l | grep -v "$croncmd" ; echo "$cronjob" ) | crontab -
+
 #droits jeedom
 sudo chown -R www-data:www-data /var/www/html
-#sudo chmod -R 775 /var/www/html
+sudo chmod -R 775 /var/www/html
+
+#d'apres la doc jeedom
+mkdir -p /var/www/html
+rm -rf /root/core-*
+wget https://github.com/jeedom/core/archive/stable.zip -O /tmp/jeedom.zip
+unzip -q /tmp/jeedom.zip -d /root/
+cp -R /root/core-*/* /var/www/html/
+cp -R /root/core-*/.htaccess /var/www/html/
 
 # redemarrage des services
-
-#service cron restart
+service cron restart
+service nginx start
