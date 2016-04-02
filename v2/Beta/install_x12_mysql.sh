91da29ed1f7c727e3e77491b7af51cd13c0240ce
megastorage <stephane.flavigny@gmail.com>
2016-04-02 23:13:10 +0200

    Update install_x12_mysql.sh

--- v2/Beta/install_x12_mysql.sh
+++ v2/Beta/install_x12_mysql.sh
@@ -334,7 +334,7 @@ configure_mysql() {
 			echo "${msg_port_changing}";
 			echo ""; echo "";
 			service mysql stop
-			sed -i 's/port = 3306/port = '"$answer"';/g /etc/mysql/my.cnf
+			sed -i 's/port = 3306/port = '"$answer"';/g /etc/mysql/my.cnf'
 			
 			
 
