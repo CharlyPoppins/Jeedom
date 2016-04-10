3eb3379d24f34df3c49bba2b254a487145954744
PuNiSHeR374 <xpunisher@live.be>
2016-04-10 17:18:57 +0200

    Update Directory

--- v2/Synology/Install/create_alias.sh
+++ v2/Synology/Install/create_alias.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/cp210x.ko
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/ftdi_sio.ko
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/usbserial.ko
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/S99Modules.sh
-mv *.ko /lib/modules
-mv S99Modules.sh /usr/syno/etc.defaults/rc.d/
-chmod 777 /usr/syno/etc.defaults/rc.d/S99Modules.sh
 cp /bin/ash /usr/local/debian-chroot/var/chroottarget/bin/
 echo 'alias debian="chroot /usr/local/debian-chroot/var/chroottarget/ /bin/bash"' >> ~/.profile
 rm /volume1/debian
--- v2/Synology/Install/jeedom.sh
+++ v2/Synology/Install/jeedom.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
-# Jeedom  chroot V2.1.X Syno X12
+# Jeedom  chroot V2.x.x
 #stef74
 case "$1" in
    "start")
    /etc/init.d/rsyslog start
-   #/etc/init.d/mtab.sh start
    /etc/init.d/cron start
-   /etc/init.d/nginx start
+   /etc/init.d/apache2 start
    /etc/init.d/php5-fpm start
    chmod 777 /dev/tty*
    ;;
    "stop")
    /etc/init.d/php5-fpm stop
-   /etc/init.d/nginx stop
+   /etc/init.d/apache2 stop
    /etc/init.d/cron stop
-   #/etc/init.d/mtab.sh stop
    /etc/init.d/rsyslog stop
    ;;
 esac
