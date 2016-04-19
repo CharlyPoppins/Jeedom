b0b193ac026cb744e29261454b5cbb16423a56ce
PuNiSHeR374 <xpunisher@live.be>
2016-04-19 12:33:26 +0200

    Update

--- v2/Synology/DSM5/Install/create_alias.sh
+++ v2/Synology/DSM5/Install/create_alias.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+
 cp /bin/ash /usr/local/debian-chroot/var/chroottarget/bin/
+
 echo 'alias debian="chroot /usr/local/debian-chroot/var/chroottarget/ /bin/bash"' >> ~/.profile
+
 rm /volume1/debian
+
 ln -s /usr/local/debian-chroot/var/chroottarget/ /volume1/debian
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/jeedom.zip
-unzip jeedom.zip -d /volume1/@appstore
+
+wget https://github.com/PuNiSHeR374/Jeedom/raw/master/v2/Synology/DSM5/Install/SynoIconShortcut.zip
+
+unzip SynoIconShortcut.zip -d /volume1/@appstore
+
 ln -s /volume1/@appstore/jeedom/ui /usr/syno/synoman/webman/3rdparty/jeedom
-rm jeedom.zip
+
+rm SynoIconShortcut.zip
--- v2/Synology/DSM6/Install/create_alias.sh
+++ v2/Synology/DSM6/Install/create_alias.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+
 cp /bin/ash /usr/local/debian-chroot/var/chroottarget/bin/
+
 echo 'alias debian="chroot /usr/local/debian-chroot/var/chroottarget/ /bin/bash"' >> ~/.profile
+
 rm /volume1/debian
+
 ln -s /usr/local/debian-chroot/var/chroottarget/ /volume1/debian
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/jeedom.zip
-unzip jeedom.zip -d /volume1/@appstore
+
+wget https://github.com/PuNiSHeR374/Jeedom/raw/master/v2/Synology/DSM6/Install/SynoIconShortcut.zip
+
+7z x SynoIconShortcut.zip -o/volume1/@appstore
+
 ln -s /volume1/@appstore/jeedom/ui /usr/syno/synoman/webman/3rdparty/jeedom
-rm jeedom.zip
+
+rm SynoIconShortcut.zip
