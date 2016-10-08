65cad3d07a539002fde4f5c72f247e8cd01d4a52
megastorage <stephane.flavigny@gmail.com>
2016-10-08 11:52:26 +0200

    Update create_alias.sh

--- v2/Synology/DSM6/Install/create_alias.sh
+++ v2/Synology/DSM6/Install/create_alias.sh
@@ -2,7 +2,7 @@
 
 cp /bin/ash /usr/local/debian-chroot/var/chroottarget/bin/
 
-alias debian="sudo -i chroot /usr/local/debian-chroot/var/chroottarget/ /bin/bash"
+alias debian="sudo -i chroot /usr/local/debian-chroot/var/chroottarget/ /bin/bash" >> ~/.profile
 
 rm /volume1/debian
 
