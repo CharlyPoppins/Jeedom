eb603defe77493290a460eaf79e6b3c39d870a74
PuNiSHeR374 <xpunisher@live.be>
2016-10-18 07:31:25 +0200

    Update install_nodejs.sh

--- v2/Synology/DSM5/Chroot/install_nodejs.sh
+++ v2/Synology/DSM5/Chroot/install_nodejs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 apt-get update
-apt-get install sudo locales curl build-essential
+apt-get -y install sudo locales curl build-essential
 
 cd /tmp
 
-echo "Début de l'installation"
+echo "Installation de Node JS"
 
 actual=`nodejs -v`;
 
@@ -17,35 +17,33 @@ then
 else
 	echo "KO, version obsolète à upgrader";
 	echo "Suppression du Nodejs existant et installation du paquet recommandé"
-	sudo apt-get -y --purge autoremove nodejs* npm*
-
+	sudo apt-get -y --purge autoremove nodejs npm
 	arch=`arch`;
-
+	echo 30 > /tmp/espeasy_dep
 	if [[ $arch == "armv6l" ]]
 	then
 		echo "Raspberry 1 détecté, utilisation du paquet pour armv6"
 		sudo rm /etc/apt/sources.list.d/nodesource.list
 		wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 		sudo dpkg -i node_latest_armhf.deb
-		sudo ln -s /usr/local/bin/nodejs /usr/local/bin/node
+		sudo ln -s /usr/local/bin/node /usr/local/bin/nodejs
 		rm node_latest_armhf.deb
-	elif [[ $arch == "x86_64" ]]
+	fi
+
+	if [[ $arch == "aarch64" ]]
 	then
-		echo "Utilisation du Code Source"
-		wget https://nodejs.org/dist/v5.9.0/node-v5.9.0.tar.gz
-		tar -xvzf node-v5.9.0.tar.gz
+		wget http://dietpi.com/downloads/binaries/c2/nodejs_5-1_arm64.deb
+		sudo dpkg -i nodejs_5-1_arm64.deb
+		sudo ln -s /usr/local/bin/node /usr/local/bin/nodejs
+		rm nodejs_5-1_arm64.deb
+	fi
 
-		cd node-v5.9.0
-
-		./configure
-		make
-		sudo make install
-	else
+	if [[ $arch != "aarch64" && $arch != "armv6l" ]]
+	then
 		echo "Utilisation du dépot officiel"
 		curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 		sudo apt-get install -y nodejs
 	fi
-
 	new=`nodejs -v`;
 	echo "Version actuelle : ${new}"
 fi
