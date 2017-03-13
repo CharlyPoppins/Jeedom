a6e54f3f903dbab3ca51cbad2f40041a1a079645
PuNiSHeR374 <xpunisher@live.be>
2017-03-13 04:31:43 +0100

    Update PatchCache.sh

--- v2/Patch/PatchCache.sh
+++ v2/Patch/PatchCache.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# sudo wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/PatchCache.sh -O /home/PatchCache.sh && chmod +x /home/PatchCache.sh && sh /home/PatchCache.sh
+# wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/PatchCache.sh -O /home/PatchCache.sh && chmod +x /home/PatchCache.sh && sh /home/PatchCache.sh
 
 
 echo ""; echo "";
@@ -16,8 +16,8 @@ echo ""
 
 sleep 3
 
-classDir=$(find /var/www -iname class -type d | egrep -v "tests|plugins|ajax")
-installDir=$(find /var/www -iname install -type d)
+classDir="/var/www/html/core/class"
+installDir="/var/www/html/install"
 
 echo ""; echo "";
 echo "**********************************************************"
@@ -27,7 +27,7 @@ echo ""
 
 echo "Backup de Jeedom en Cours..."
 
-sudo php ${installDir}/backup.php >> ${installDir}/PatchLog.txt
+php ${installDir}/backup.php >> ${installDir}/PatchLog.txt
 
 echo ""; echo "";
 echo "**********************************************************"
@@ -39,14 +39,14 @@ echo "Les fichiers concernés vont être renommés en .bck"
 
 sleep 3
 
-sudo chmod 777 -R /var/www
+chmod 777 -R /var/www
 
-sudo cp -v -p ${classDir}/cache.class.php ${classDir}/cache.class.php.bck >> ${installDir}/PatchLog.txt
-sudo cp -v -p ${classDir}/cmd.class.php ${classDir}/cmd.class.php.bck >> ${installDir}/PatchLog.txt
-sudo cp -v -p ${classDir}/cron.class.php ${classDir}/cron.class.php.bck >> ${installDir}/PatchLog.txt
-sudo cp -v -p ${classDir}/eqLogic.class.php ${classDir}/eqLogic.class.php.bck >> ${installDir}/PatchLog.txt
-sudo cp -v -p ${classDir}/scenario.class.php ${classDir}/scenario.class.php.bck >> ${installDir}/PatchLog.txt
-sudo cp -v -p ${installDir}/consistency.php ${installDir}/consistency.php.bck >> ${installDir}/PatchLog.txt
+cp -v -p ${classDir}/cache.class.php ${classDir}/cache.class.php.bck >> ${installDir}/PatchLog.txt
+cp -v -p ${classDir}/cmd.class.php ${classDir}/cmd.class.php.bck >> ${installDir}/PatchLog.txt
+cp -v -p ${classDir}/cron.class.php ${classDir}/cron.class.php.bck >> ${installDir}/PatchLog.txt
+cp -v -p ${classDir}/eqLogic.class.php ${classDir}/eqLogic.class.php.bck >> ${installDir}/PatchLog.txt
+cp -v -p ${classDir}/scenario.class.php ${classDir}/scenario.class.php.bck >> ${installDir}/PatchLog.txt
+cp -v -p ${installDir}/consistency.php ${installDir}/consistency.php.bck >> ${installDir}/PatchLog.txt
 
 echo ""; echo "";
 echo "**********************************************************"
@@ -56,12 +56,12 @@ echo ""
 
 echo "Téléchargement et remplacement des fichiers en Cours..."
 
-sudo wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/cache.class.php -O ${classDir}/cache.class.php >> ${installDir}/PatchLog.txt
-sudo wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/cmd.class.php -O ${classDir}/cmd.class.php >> ${installDir}/PatchLog.txt
-sudo wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/cron.class.php -O ${classDir}/cron.class.php >> ${installDir}/PatchLog.txt
-sudo wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/eqLogic.class.php -O ${classDir}/eqLogic.class.php >> ${installDir}/PatchLog.txt
-sudo wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/scenario.class.php -O ${classDir}/scenario.class.php >> ${installDir}/PatchLog.txt
-sudo wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/consistency.php -O ${installDir}/consistency.php >> ${installDir}/PatchLog.txt
+wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/cache.class.php -O ${classDir}/cache.class.php >> ${installDir}/PatchLog.txt
+wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/cmd.class.php -O ${classDir}/cmd.class.php >> ${installDir}/PatchLog.txt
+wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/cron.class.php -O ${classDir}/cron.class.php >> ${installDir}/PatchLog.txt
+wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/eqLogic.class.php -O ${classDir}/eqLogic.class.php >> ${installDir}/PatchLog.txt
+wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/scenario.class.php -O ${classDir}/scenario.class.php >> ${installDir}/PatchLog.txt
+wget --no-check-certificate https://raw.githubusercontent.com/PuNiSHeR374/Jeedom/master/v2/Patch/consistency.php -O ${installDir}/consistency.php >> ${installDir}/PatchLog.txt
 
 echo ""; echo "";
 echo "**********************************************************"
@@ -72,7 +72,7 @@ echo ""
 echo "Veuillez patienter, cela peut durer +/- 2 minutes."
 echo ""
 
-sudo php ${installDir}/consistency.php >> ${installDir}/PatchLog.txt
+php ${installDir}/consistency.php >> ${installDir}/PatchLog.txt
 
 echo ""; echo "";
 echo "**********************************************************"
@@ -80,8 +80,8 @@ echo "Mise à plat des droits."
 echo "**********************************************************"
 echo ""
 
-sudo chmod 775 -R /var/www
-sudo chown -R www-data:www-data /var/www
+chmod 775 -R /var/www
+chown -R www-data:www-data /var/www
 
 sleep 2
 
@@ -97,4 +97,4 @@ echo "En Cours..."
 
 sleep 2
 
-sudo reboot
+reboot
