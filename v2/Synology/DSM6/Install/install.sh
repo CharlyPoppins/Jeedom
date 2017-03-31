1e26423149e4df31b114e18390d42e54d3f7c855
megastorage <stephane.flavigny@gmail.com>
2017-03-31 07:41:42 +0200

    Update install.sh

--- v2/Synology/DSM6/Install/install.sh
+++ v2/Synology/DSM6/Install/install.sh
@@ -110,12 +110,26 @@ ln -s /etc/apache2/conf-available/security.conf /etc/apache2/conf-enabled/
 rm /var/www/html/index.html
 
 #config apache2
-sed -i 's/max_execution_time = 30/max_execution_time = 600/g' /etc/php5/apache2/php.ini
-sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1G/g' /etc/php5/apache2/php.ini
-sed -i 's/post_max_size = 8M/post_max_size = 1G/g' /etc/php5/apache2/php.ini
-sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/apache2/php.ini
-sed -i 's/pm.max_children = 5/pm.max_children = 20/g' /etc/php5/fpm/pool.d/www.conf
+//sed -i 's/max_execution_time = 30/max_execution_time = 600/g' /etc/php5/apache2/php.ini
+//sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1G/g' /etc/php5/apache2/php.ini
+//sed -i 's/post_max_size = 8M/post_max_size = 1G/g' /etc/php5/apache2/php.ini
+//sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/apache2/php.ini
+//sed -i 's/pm.max_children = 5/pm.max_children = 20/g' /etc/php5/fpm/pool.d/www.conf
 
+	for file in ` find / -name php.ini -type f`;do
+		echo "Update php file ${file}"
+		sed -i 's/max_execution_time = 30/max_execution_time = 600/g' ${file} > /dev/null 2>&1
+	    sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1G/g' ${file} > /dev/null 2>&1
+	    sed -i 's/post_max_size = 8M/post_max_size = 1G/g' ${file} > /dev/null 2>&1
+	    sed -i 's/expose_php = On/expose_php = Off/g' ${file} > /dev/null 2>&1
+	    sed -i 's/;opcache.enable=0/opcache.enable=1/g' ${file} > /dev/null 2>&1
+	    sed -i 's/opcache.enable=0/opcache.enable=1/g' ${file} > /dev/null 2>&1
+	    sed -i 's/;opcache.enable_cli=0/opcache.enable_cli=1/g' ${file} > /dev/null 2>&1
+	    sed -i 's/opcache.enable_cli=0/opcache.enable_cli=1/g' ${file} > /dev/null 2>&1
+	done
+	
+	a2dismod status
+	systemctl restart apache2 > /dev/null 2>&1
 
 #config des droits	
 echo "www-data ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)
